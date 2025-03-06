9f27a088f037bb62ec4834465d860f737bfb32be
Sam Teater <samuelteater@gmail.com>
2025-03-06 17:35:11 -0500

    Fixed Calculations

--- script.js
+++ script.js
@@ -28,40 +28,48 @@ function validatePsiInput() {
       input.value = 10; // Set to maximum
     }
   }
+  function validateVolumeInput() {
+    var input = document.getElementById("volumeInput");
+    var value = parseInt(input.value, 10);
+    
+    if (value < 0) {
+      input.value = 0; // Set to minimum
+    } else if (value > 100) {
+      input.value = 100; // Set to maximum
+    }
+  }
 document.getElementById("calculateButton").addEventListener("click", function() {
-    var F = 0
-    var a = 0;
     var v = 0;
-    var vx = 0.7071067811865476;
-    var vy = 0.7071067811865476;
-    var tUp = 0;
-    var tTotal = 0;
     var g = 9.81;
     var R = 0;
+    var vBarrel = 0;
+    var V = 0;
+    var t = 0;
     
     let P = document.getElementById("psiInput")
     let A = document.getElementById("areaInput")
     let m = document.getElementById("massInput")
+    let vTank = document.getElementById("volumeInput")
     P = parseFloat(P.value) * 6894.76; // Convert PSI to Pascals
     A = parseFloat(A.value) * 0.00064516; // Convert square inches to square meters
-    m = parseFloat(m.value); // Convert mass to a number
-    F = P*A;
+    m = parseFloat(m.value) * 0.453; // Convert mass to kg 
+    vTank = parseFloat(vTank.value) * 0.0000163871; //convert the tank volume to cubic meters
 
-    a = F/m;
-  
-    v = a*0.4;
+    V = vTank/(vTank+vBarrel);
+    V = Math.pow(V, 1.4);
+    P = P*V;
+
+    v = 4 * P * A * 0.4;
+    v = v/2;
     v = Math.sqrt(v);
-  
-    vx = vx*v;
-    vy = vy*v;
-  
-    tUp = vy/g;
-    tTotal = 2*tUp;
-  
-    R = vx*tTotal;
-    R = R*3.28; //Convert meters to feet
-    tTotal = Math.round(Number(tTotal));
+
+    R = (v*v)/g;
+    R = R*3.28;
+    t = R/v;
+    t = t/3.28
+
+    t = Math.round(Number(t));
     R = Math.round(R);
     document.getElementById("distanceLabel").textContent = "DISTANCE = "+R+" FT";
-    document.getElementById("timeLabel").textContent = "TIME = "+tTotal+" S";
+    document.getElementById("timeLabel").textContent = "TIME = "+t+" S";
 });

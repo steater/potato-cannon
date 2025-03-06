0aa4a46beb72e54101d7e9104368296f4c5478af
Sam Teater <samuelteater@gmail.com>
2025-03-05 21:43:32 -0500

    number restrictions and time output

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+function validatePsiInput() {
+    var input = document.getElementById("psiInput");
+    var value = parseInt(input.value, 10);
+    
+    if (value < 1) {
+      input.value = 1; // Set to minimum
+    } else if (value > 100) {
+      input.value = 100; // Set to maximum
+    }
+  }
+  function validateMassInput() {
+    var input = document.getElementById("massInput");
+    var value = parseInt(input.value, 10);
+    
+    if (value < 0) {
+      input.value = 0; // Set to minimum
+    } else if (value > 10) {
+      input.value = 10; // Set to maximum
+    }
+  }
+  function validateAreaInput() {
+    var input = document.getElementById("areaInput");
+    var value = parseInt(input.value, 10);
+    
+    if (value < 0) {
+      input.value = 0; // Set to minimum
+    } else if (value > 10) {
+      input.value = 10; // Set to maximum
+    }
+  }
 document.getElementById("calculateButton").addEventListener("click", function() {
     var F = 0
     var a = 0;
@@ -16,7 +46,7 @@ document.getElementById("calculateButton").addEventListener("click", function() 
     A = parseFloat(A.value) * 0.00064516; // Convert square inches to square meters
     m = parseFloat(m.value); // Convert mass to a number
     F = P*A;
-    console.log(F);
+
     a = F/m;
   
     v = a*0.4;
@@ -32,4 +62,5 @@ document.getElementById("calculateButton").addEventListener("click", function() 
     R = R*3.28; //Convert meters to feet
     R = Math.round(R);
     document.getElementById("distanceLabel").textContent = "DISTANCE = "+R+" FT";
+    document.getElementById("timeLabel").textContent = "TIME = "+tTotal+" S";
 });
